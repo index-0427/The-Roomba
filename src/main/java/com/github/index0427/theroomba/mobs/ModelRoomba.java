9993ce891e2c13ad7c52b8aedb3518e29b65ec40
index-0427 <magnolia413xue@gmail.com>
2022-05-26 22:17:37 +0900

    テクスチャ修正

--- src/main/java/com/github/index0427/theroomba/mobs/ModelRoomba.java
+++ src/main/java/com/github/index0427/theroomba/mobs/ModelRoomba.java
@@ -4,31 +4,28 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
+/**
+ * roomba3 - Undefined
+ * Created using Tabula 7.1.0
+ */
 public class ModelRoomba extends ModelBase {
-//    public ModelRoomba() { }
-
     public ModelRenderer shape1;
     public ModelRenderer shape2;
-    public ModelRenderer shape3;
 
     public ModelRoomba() {
         this.textureWidth = 128;
-        this.textureHeight = 64;
-        this.shape3 = new ModelRenderer(this, 0, 0);
-        this.shape3.setRotationPoint(0.0F, 18.3F, 7.6F);
-        this.shape3.addBox(-0.5F, -0.5F, 0.0F, 1, 1, 6, 0.0F);
-        this.setRotateAngle(shape3, 0.2617993877991494F, 0.0F, 0.0F);
-        this.shape2 = new ModelRenderer(this, 0, 15);
-        this.shape2.setRotationPoint(0.0F, 18.0F, 0.0F);
-        this.shape2.addBox(-3.5F, -1.7F, -2.2000000000000006F, 7, 7, 10, 0.0F);
-        this.shape1 = new ModelRenderer(this, 4, 0);
-        this.shape1.setRotationPoint(0.0F, 18.0F, 0.0F);
-        this.shape1.addBox(-5.0F, -4.5F, -12.199999999999992F, 10, 10, 10, 0.0F);
+        this.textureHeight = 128;
+        this.shape2 = new ModelRenderer(this, 32, 0);
+        this.shape2.setRotationPoint(-1.7F, 0.0F, 4.0F);
+        this.shape2.addBox(0.0F, 0.0F, 0.0F, 8, 2, 8, 0.0F);
+        this.setRotateAngle(shape2, 0.0F, 0.7853981633974483F, 0.0F);
+        this.shape1 = new ModelRenderer(this, 0, 0);
+        this.shape1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.shape1.addBox(0.0F, 0.0F, 0.0F, 8, 2, 8, 0.0F);
     }
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-        this.shape3.render(f5);
         this.shape2.render(f5);
         this.shape1.render(f5);
     }
